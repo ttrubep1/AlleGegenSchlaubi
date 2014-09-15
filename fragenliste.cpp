c800803a97e13831839f6ebfab254d32f84636b9
Tobias Triffterer <tobias@ep1.ruhr-uni-bochum.de>
2014-09-15 22:16:21 +0200

    Qt-Listenmodell für Fragen
    Die Klasse "FragenListe" verwaltet einen Vektor mit Frage-Objekten und
    ist dabei gleichzeitig ein Kind von QAbstractListModel, so dass die
    Liste der Fragen in einem entsprechenden Qt-Steuerelement angezeigt
    werden kann.

---
+++ fragenliste.cpp
@@ -0,0 +1,138 @@
+/*
+ * AlleGegenSchlaubi - educational quiz game
+ * Copyright (C) 2014  Tobias Triffterer <tobias@ep1.ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdexcept>
+
+#include "fragenliste.h"
+
+FragenListe::FragenListe() : QAbstractListModel()
+{
+
+}
+
+FragenListe::~FragenListe()
+{
+
+}
+
+QVariant FragenListe::data ( const QModelIndex& queriedindex, int queriedrole ) const
+{
+    switch ( queriedrole )
+    {
+    case Qt::DisplayRole:
+        if ( queriedindex == QModelIndex() )
+            return QVariant();
+        if ( ( size_t ) queriedindex.row() >= _fragen.size() -1 )
+            throw std::out_of_range ( "queriedindex.row() zu groß in FragenListe::data()" );
+        return QVariant::fromValue<QString> ( _fragen[queriedindex.row()].getTitel() );
+        break;
+    case Qt::UserRole:
+        if ( queriedindex == QModelIndex() )
+            return QVariant();
+        if ( ( size_t ) queriedindex.row() >= _fragen.size() )
+            throw std::out_of_range ( "queriedindex.row() zu groß in FragenListe::data()" );
+        return QVariant::fromValue<int> ( queriedindex.row() );
+        break;
+    default:
+        return QVariant();
+    }
+}
+
+int FragenListe::rowCount ( const QModelIndex& parentindex ) const
+{
+    Q_UNUSED ( parentindex );
+    return ( int ) _fragen.size();
+}
+
+void FragenListe::neueFrage ( const Frage& frage )
+{
+    beginInsertRows ( QModelIndex(), ( int ) _fragen.size(), ( int ) _fragen.size() );
+    _fragen.push_back ( frage );
+    endInsertRows();
+}
+
+void FragenListe::neueFrage ( Frage&& frage )
+{
+    beginInsertRows ( QModelIndex(), ( int ) _fragen.size(), ( int ) _fragen.size() );
+    _fragen.push_back ( std::move ( frage ) );
+    endInsertRows();
+}
+
+Frage& FragenListe::holeFrage ( const size_t nummer )
+{
+    if ( nummer >= _fragen.size() )
+        throw std::out_of_range ( "Fragennummer zu groß in FragenListe::holeFrage()" );
+    return _fragen[nummer];
+}
+
+void FragenListe::loescheFrage ( const size_t nummer )
+{
+    if ( nummer >= _fragen.size() )
+        throw std::out_of_range ( "Fragennummer zu groß in FragenListe::loescheFrage()" );
+    beginRemoveRows ( QModelIndex(), ( int ) nummer, ( int ) nummer );
+    _fragen.erase ( _fragen.begin() + nummer );
+    endRemoveRows();
+}
+
+void FragenListe::loescheAlles()
+{
+    beginRemoveRows ( QModelIndex(), 0, ( int ) _fragen.size() );
+    _fragen.clear();
+    endRemoveRows();
+}
+
+void FragenListe::schiebeFrageHoch ( const size_t nummer )
+{
+    if ( nummer >= _fragen.size() )
+        throw std::out_of_range ( "Fragennummer zu groß in FragenListe::schiebeFrageHoch()" );
+    if ( nummer == 0 )
+        return; // Erste Frage kann nicht hochgeschoben werden
+    std::swap<Frage> ( _fragen[nummer], _fragen[nummer-1] );
+    emit dataChanged ( index ( ( int ) nummer-1 ), index ( ( int ) nummer ) );
+}
+
+void FragenListe::schiebeFrageRunter ( const size_t nummer )
+{
+    if ( nummer >= _fragen.size() )
+        throw std::out_of_range ( "Fragennummer zu groß in FragenListe::schiebeFrageHoch()" );
+    if ( nummer == _fragen.size() -1 )
+        return; // Letzte Frage kann nicht runtergeschoben werden
+    std::swap<Frage> ( _fragen[nummer], _fragen[nummer+1] );
+    emit dataChanged ( index ( ( int ) nummer ), index ( ( int ) nummer+1 ) );
+}
+
+FragenListe::FragenVektor::iterator FragenListe::begin()
+{
+    return _fragen.begin();
+}
+
+FragenListe::FragenVektor::iterator FragenListe::end()
+{
+    return _fragen.end();
+}
+
+FragenListe::FragenVektor::const_iterator FragenListe::begin() const
+{
+    return _fragen.begin();
+}
+
+FragenListe::FragenVektor::const_iterator FragenListe::end() const
+{
+    return _fragen.end();
+}
---
+++ fragenliste.h
@@ -0,0 +1,55 @@
+/*
+ * AlleGegenSchlaubi - educational quiz game
+ * Copyright (C) 2014  Tobias Triffterer <tobias@ep1.ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef FRAGENLISTE_H
+#define FRAGENLISTE_H
+
+#include <QAbstractItemModel>
+
+#include "frage.h"
+
+class FragenListe : public QAbstractListModel
+{
+public:
+    typedef std::vector<Frage> FragenVektor;
+private:
+    FragenVektor _fragen;
+public:
+    FragenListe();
+    virtual ~FragenListe();
+    virtual QVariant data ( const QModelIndex& queriedindex, int queriedrole ) const;
+    virtual int rowCount ( const QModelIndex& parent = QModelIndex() ) const;
+    FragenListe ( const FragenListe& other ) = delete;
+    FragenListe ( FragenListe&& other ) = delete;
+    FragenListe& operator= ( const FragenListe& other ) = delete;
+    FragenListe& operator= ( FragenListe&& other ) = delete;
+    void neueFrage ( const Frage& frage );
+    void neueFrage ( Frage&& frage );
+    Frage& holeFrage ( const size_t nummer );
+    void loescheFrage ( const size_t nummer );
+    void loescheAlles();
+    void schiebeFrageHoch ( const size_t nummer );
+    void schiebeFrageRunter ( const size_t nummer );
+    FragenListe::FragenVektor::iterator begin();
+    FragenListe::FragenVektor::iterator end();
+    FragenListe::FragenVektor::const_iterator begin() const;
+    FragenListe::FragenVektor::const_iterator end() const;
+};
+
+#endif // FRAGENLISTE_H
